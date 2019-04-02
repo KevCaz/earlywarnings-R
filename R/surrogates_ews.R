ae3c5191fd2888b57d868abf29505b623f18a465
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-04-02 13:37:40 -0400

    restructure the repo

--- R/surrogates_ews.R
+++ R/surrogates_ews.R
@@ -15,8 +15,8 @@
 #'    @param degree the degree of polynomial to be used for when loess fitting is applied, normally 1 or 2 (Default). see more on loess{stats}
 #'    @param boots the number of surrogate data. Default is 100.
 #'    @param logtransform logical. If TRUE data are logtransformed prior to analysis as log(X+1). Default is FALSE.
-#'    @param interpolate logical. If TRUE linear interpolation is applied to produce a timeseries of equal length as the original. Default is FALSE (assumes there are no gaps in the timeseries). 
-#' 
+#'    @param interpolate logical. If TRUE linear interpolation is applied to produce a timeseries of equal length as the original. Default is FALSE (assumes there are no gaps in the timeseries).
+#'
 # Returns:
 #'   @return \code{surrogates_ews} returns a matrix that contains:
 #'   @return \item{Kendall tau estimate original}{the trends of the original timeseries.}
@@ -26,35 +26,35 @@
 #'   @return \item{significance p}{the p-value for the original Kendall tau rank correlation estimate compared to the surrogates.}
 #'
 #' In addition, \code{surrogates_ews} returns a plot with the distribution of the surrogate Kendall tau estimates and the Kendall tau estimate of the original series. Vertical lines indicate the 5\% and 95\% significance levels.
-#'  
+#'
 #' @export
-#' 
+#'
 #' @author Vasilis Dakos \email{vasilis.dakos@@gmail.com}
-#' @references Dakos, V., et al (2008). 'Slowing down as an early warning signal for abrupt climate change.' \emph{Proceedings of the National Academy of Sciences} 105(38): 14308-14312 
-#' 
-#' Dakos, V., et al (2012).'Methods for Detecting Early Warnings of Critical Transitions in Time Series Illustrated Using Simulated Ecological Data.' \emph{PLoS ONE} 7(7): e41010. doi:10.1371/journal.pone.0041010 
-#' @seealso 
-#' \code{\link{generic_ews}}; \code{\link{ddjnonparam_ews}}; \code{\link{bdstest_ews}}; \code{\link{sensitivity_ews}}; \code{\link{surrogates_ews}}; \code{\link{ch_ews}}; \code{\link{movpotential_ews}}; \code{\link{livpotential_ews}} 
+#' @references Dakos, V., et al (2008). 'Slowing down as an early warning signal for abrupt climate change.' \emph{Proceedings of the National Academy of Sciences} 105(38): 14308-14312
+#'
+#' Dakos, V., et al (2012).'Methods for Detecting Early Warnings of Critical Transitions in Time Series Illustrated Using Simulated Ecological Data.' \emph{PLoS ONE} 7(7): e41010. doi:10.1371/journal.pone.0041010
+#' @seealso
+#' \code{\link{generic_ews}}; \code{\link{ddjnonparam_ews}}; \code{\link{bdstest_ews}}; \code{\link{sensitivity_ews}}; \code{\link{surrogates_ews}}; \code{\link{ch_ews}}; \code{\link{movpotential_ews}}; \code{\link{livpotential_ews}}
 # ; \code{\link{timeVAR_ews}}; \code{\link{thresholdAR_ews}}
-#' @examples 
-#' data(foldbif) 
+#' @examples
+#' data(foldbif)
 #' output=surrogates_ews(foldbif,indicator='sd',winsize=50,detrending='gaussian',
 #' bandwidth=10,boots=200,logtransform=FALSE,interpolate=FALSE)
 #' @keywords early-warning
 
 # Author: Vasilis Dakos, January 4, 2012
 
-surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", "kurt", 
-    "cv", "returnrate", "densratio"), winsize = 50, detrending = c("no", "gaussian", 
-    "loess", "linear", "first-diff"), bandwidth = NULL, span = NULL, degree = NULL, 
+surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", "kurt",
+    "cv", "returnrate", "densratio"), winsize = 50, detrending = c("no", "gaussian",
+    "loess", "linear", "first-diff"), bandwidth = NULL, span = NULL, degree = NULL,
     boots = 100, logtransform = FALSE, interpolate = FALSE) {
-    
+
     # timeseries<-ts(timeseries) #strict data-types the input data as tseries object
     # for use in later steps
-    
+
     skewness <- moments::skewness
     kurtosis <- moments::kurtosis
-    
+
     timeseries <- data.matrix(timeseries)
     if (dim(timeseries)[2] == 1) {
         Y = timeseries
@@ -65,7 +65,7 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
     } else {
         warning("not right format of timeseries input")
     }
-    
+
     # Interpolation
     if (interpolate) {
         YY <- approx(timeindex, Y, n = length(Y), method = "linear")
@@ -73,12 +73,12 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
     } else {
         Y <- Y
     }
-    
+
     # Log-transformation
     if (logtransform) {
         Y <- log(Y + 1)
     }
-    
+
     # Detrending
     detrending <- match.arg(detrending)
     if (detrending == "gaussian") {
@@ -87,7 +87,7 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
         } else {
             bw <- round(length(Y) * bandwidth)/100
         }
-        smYY <- ksmooth(timeindex, Y, kernel = c("normal"), bandwidth = bw, range.x = range(timeindex), 
+        smYY <- ksmooth(timeindex, Y, kernel = c("normal"), bandwidth = bw, range.x = range(timeindex),
             n.points = length(timeindex))
         nsmY <- Y - smYY$y
         smY <- smYY$y
@@ -102,7 +102,7 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
         } else {
             degree <- degree
         }
-        smYY <- loess(Y ~ timeindex, span = span, degree = degree, normalize = FALSE, 
+        smYY <- loess(Y ~ timeindex, span = span, degree = degree, normalize = FALSE,
             family = "gaussian")
         smY <- predict(smYY, data.frame(x = timeindex), se = FALSE)
         nsmY <- Y - smY
@@ -116,8 +116,8 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
         smY <- Y
         nsmY <- Y
     }
-    
-    
+
+
     # Rearrange data for indicator calculation
     mw <- round(length(Y) * winsize)/100
     omw <- length(nsmY) - mw + 1
@@ -164,11 +164,11 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
     }
     # Calculate trend statistics
     timevec <- seq(1, length(indic))
-    Kt <- cor.test(timevec, indic, alternative = c("two.sided"), method = c("kendall"), 
+    Kt <- cor.test(timevec, indic, alternative = c("two.sided"), method = c("kendall"),
         conf.level = 0.95)
     Ktauestindorig <- Kt$estimate
     Ktaupindorig <- Kt$p.value
-    
+
     # Fit ARMA model based on AIC
     arma = matrix(, 4, 5)
     for (ij in 1:4) {
@@ -179,25 +179,25 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
             print(paste(ij, jj, ARMA$aic), zero.print = ".", quote = FALSE)
         }
     }
-    
+
     # Simulate ARMA(p,q) model fitted on residuals
     ind = which(arma == min(arma), arr.ind = TRUE)
     ARMA <- arima(nsmY, order = c(ind[1], 0, ind[2] - 1), include.mean = FALSE)
-    
+
     Ktauestind <- numeric()
     Ktaupind <- numeric()
-    
+
     for (jjj in 1:boots) {
-        x = arima.sim(n = length(nsmY), list(ar = c(ARMA$coef[1:ind[1]]), ma = c(ARMA$coef[(1 + 
+        x = arima.sim(n = length(nsmY), list(ar = c(ARMA$coef[1:ind[1]]), ma = c(ARMA$coef[(1 +
             ind[1]):(ind[1] + ind[2] - 1)])), sd = sqrt(ARMA$sigma2))
-        
+
         ## Rearrange data for indicator calculation
         nMR1 <- matrix(data = NA, nrow = mw, ncol = omw)
         for (i in 1:omw) {
             Ytw <- x[i:(i + mw - 1)]
             nMR1[, i] <- Ytw
         }
-        
+
         # Estimate indicator
         indicator = match.arg(indicator)
         if (indicator == "ar1") {
@@ -232,35 +232,35 @@ surrogates_ews <- function(timeseries, indicator = c("ar1", "sd", "acf1", "sk", 
                 spectfft$spec[low]/spectfft$spec[high]
             })
         }
-        
+
         # Calculate trend statistics
         timevec <- seq(1, length(indic))
-        Kt <- cor.test(timevec, indic, alternative = c("two.sided"), method = c("kendall"), 
+        Kt <- cor.test(timevec, indic, alternative = c("two.sided"), method = c("kendall"),
             conf.level = 0.95)
         Ktauestind[jjj] <- Kt$estimate
         Ktaupind[jjj] <- Kt$p.value
     }
-    
+
     # Estimate probability of false positive
     q <- sort(Ktauestind, na.last = NA)
     Kpos <- max(which(Ktauestindorig > q), na.rm = TRUE)
     p <- (boots + 1 - Kpos)/boots
     print(paste("significance p = ", p, " estimated from ", boots, " surrogate ARMA timeseries"))
-    
+
     # Plotting
     dev.new()
     par(font.main = 10, mar = (c(4.6, 4, 0.5, 2) + 0.2), oma = c(0.5, 1, 2, 1))
-    hist(Ktauestind, freq = TRUE, nclass = 20, xlim = c(-1, 1), col = "blue", main = NULL, 
-        xlab = "Surrogate Kendall tau estimates", ylab = "occurrence", ylim = c(0, 
+    hist(Ktauestind, freq = TRUE, nclass = 20, xlim = c(-1, 1), col = "blue", main = NULL,
+        xlab = "Surrogate Kendall tau estimates", ylab = "occurrence", ylim = c(0,
             boots))
     abline(v = q[0.05 * boots], col = "black", lwd = 1)
     abline(v = q[0.95 * boots], col = "black", lwd = 1)
     points(Ktauestindorig, 0, pch = 21, bg = "black", col = "black", cex = 1)
     mtext(paste("Indicator ", toupper(indicator)), side = 3, line = 0.2, outer = TRUE)
-    
+
     # Output
     out <- data.frame(Ktauestindorig, Ktaupindorig, Ktauestind, Ktaupind, p)
-    colnames(out) <- c("Kendall tau estimate original", "Kendall tau p-value original", 
+    colnames(out) <- c("Kendall tau estimate original", "Kendall tau p-value original",
         "Kendall tau estimate surrogates", "Kendall tau p-value surrogates", "significance p")
-    return(out)
-} 
+    out
+}
